11db14d063559392029ddd92e67e9c3213ffb2cd
pvial <pvial@thin>
2020-02-27 10:06:36 -0800

    hyper loadavg

--- Nhyper.py
+++ Nhyper.py
@@ -7,7 +7,11 @@ def hyperloadavg(hypers):
     salt_cmd = ['salt', '--out', 'yaml', hypers, 'status.loadavg']
     out = subprocess.check_output(salt_cmd)
     loads = yaml.load(out)
-    load = 0
+    load1 = 0
+    load5 = 0
+    load15 = 0
     for key in loads.keys():
-        load += loads[key]['1-min']
-    return load
+        load1 += loads[key]['1-min']
+        load5 += loads[key]['5-min']
+        load15 += loads[key]['15-min']
+    return [load1, load5, load15]
